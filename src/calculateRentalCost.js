ae428c1bbde6c775f31d4e8cc1735e9798f61ee2
rokas <rokster112@gmail.com>
2025-02-17 16:07:30 +0200

    fixing AI Buddy errors

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,10 +5,13 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const perDayCost = 40
+  const sevenDayDiscount = 50
+  const threeDayDiscount = 20
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * perDayCost - sevenDayDiscount;
   } else if (days >= 3) {
-    return days * 40 - 20;
+    return days * perDayCost - threeDayDiscount;
   }
 
   return days * 40;
